2f416ca2b6a77a2c715388fa3092c2c892cd85d0
J-C <jcjoneskern@gmail.com>
2016-11-13 18:34:32 -0500

    adding selectable plugin

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ $(function() { //equivalent to document.ready
        $("#form").slideDown().show();
        seatNum = event.target.id;
        currentSeat = $(this);
-      $(currentSeat).addClass("active");
+      // $(currentSeat).addClass("active");
    }
    });
    $("#submit").on("click", function() {
@@ -22,7 +22,7 @@ $(function() { //equivalent to document.ready
        {name: name,
        number: seatNum}
      );
-     $(currentSeat).addClass("reserved").removeClass("active available");
+     $(currentSeat).addClass("reserved").removeClass("available"); //removed active from this, might need to be added back
      $(currentSeat).text("Reserved");
      $("#nameField").val("");
      $("#thanks").show().html("<p>Thank you for your reservation, "+name+"!");
@@ -57,21 +57,22 @@ $(function() { //equivalent to document.ready
  });
  //end price info
  //this doesn't necessarily need to be implemented with hover, but I think it looks ~fancy~
+ $("#selectable").selectable();
 
  //adding the active class doesn't really do what we want, so we should try to figure out the selectable UI probably
 //SELECTABLE SEAT FUNCTION
 
   //THIS FUNCTION ALLOWS YOU TO RESERVE THE SEAT UPON CLICK...SEAT A1-A6
-    $("#A1, #A2, #A3, #A4, #A5, #A6").click(function () {
+    // $("#A1, #A2, #A3, #A4, #A5, #A6").click(function () {
         // $(this).css('background', 'goldenrod')
         // $(this).css('font-family', 'Cuprum')
         // $(this).css("color", " whitesmoke");
-        $(this).text("V.I.P");
+        // $(this).text("V.I.P");
         // $(this).animate({
         //     borderRadius: "9px"
         //     , fontSize: "1.5em"
         // })
-    });
+    // });
 
     //THIS FUNCTION TELLS YOU HOW MUCH EACH SEAT IS UPON MOUSEENTER....SEAT A1-A6
     //    $("#A1, #A2, #A3, #A4, #A5, #A6").mouseenter(function () {
@@ -86,16 +87,16 @@ $(function() { //equivalent to document.ready
 
   //THIS FUNCTION ALLOWS YOU TO RESERVE THE SEAT UPON CLICK...SEAT B1-B6
 
-    $("#B1, #B2, #B3, #B4, #B5, #B6").click(function () {
+    // $("#B1, #B2, #B3, #B4, #B5, #B6").click(function () {
         // $(this).css('background', 'silver')
         // $(this).css('font-family', 'Cuprum')
         // $(this).css("color", " orangered");
-        $(this).text("general admission");
+        // $(this).text("general admission");
         // $(this).animate({
         //     borderRadius: "9px"
         //     , fontSize: "1.8em"
         // })
-    });
+    // });
 
     //THIS FUNCTION TELLS YOU HOW MUCH EACH SEAT IS UPON MOUSEENTER....SEAT B1-B6
     //    $("#B1, #B2, #B3, #B4, #B5, #B6").mouseenter(function () {
@@ -109,16 +110,16 @@ $(function() { //equivalent to document.ready
     // });
 
   //THIS FUNCTION ALLOWS YOU TO RESERVE THE SEAT UPON CLICK...SEAT C1-C6
-    $("#C1, #C2, #C3, #C4, #C5, #C6").click(function () {
+    // $("#C1, #C2, #C3, #C4, #C5, #C6").click(function () {
         // $(this).css('background', 'brown')
         // $(this).css('font-family', 'Cuprum')
         // $(this).css("color", " orangered");
-        $(this).text("general admission");
+        // $(this).text("general admission");
         // $(this).animate({
         //     borderRadius: "9px"
         //     , fontSize: "1.8em"
         // })
-    });
+    // });
 
     //THIS FUNCTION TELLS YOU HOW MUCH EACH SEAT IS UPON MOUSEENTER....SEAT C1-C6
     //    $("#C1, #C2, #C3, #C4, #C5, #C6").mouseenter(function () {
@@ -132,16 +133,16 @@ $(function() { //equivalent to document.ready
     // });
 
   //THIS FUNCTION ALLOWS YOU TO RESERVE THE SEAT UPON CLICK...SEAT C1-C6
-    $("#D1, #D2, #D3, #D4, #D5, #D6").click(function () {
+    // $("#D1, #D2, #D3, #D4, #D5, #D6").click(function () {
         // $(this).css('background', 'darkslateblue')
         // $(this).css('font-family', 'Cuprum')
         // $(this).css("color", " orangered");
-        $(this).text("general admission");
+        // $(this).text("general admission");
         // $(this).animate({
         //     borderRadius: "9px"
         //     , fontSize: "1.8em"
         // })
-    });
+    // });
 
     //THIS FUNCTION TELLS YOU HOW MUCH EACH SEAT IS UPON MOUSEENTER....SEAT C1-C6
     //  $("#D1, #D2, #D3, #D4, #D5, #D6").mouseenter(function () {
