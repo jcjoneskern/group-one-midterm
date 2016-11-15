444a4a07750e23bee60962b87a28d183a6a2c54f
J-C <jcjoneskern@gmail.com>
2016-11-14 23:11:48 -0500

    done?!

--- Slides-SlidesJS-3/Slides-SlidesJS-3/source/jquery.slides.js
+++ Slides-SlidesJS-3/Slides-SlidesJS-3/source/jquery.slides.js
@@ -212,9 +212,8 @@
         zIndex: 0
       });
       width = $element.width();
-      height = (this.options.height / this.options.width) * width;
+      height = 480;
       this.options.width = width;
-      // this.options.height = height;
       return $(".slidesjs-control, .slidesjs-container", $element).css({
         width: width,
         height: height
